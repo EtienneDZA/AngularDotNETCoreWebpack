1f415f7592ce3a40b67f0f4ec93dd19ab964a6dd
Etienne de Lange <etiennedelange@live.co.za>
2018-03-17 14:06:01 +0200

    Initial commit

---
+++ AngularMVCApplication/App/app.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component, OnInit } from '@angular/core'
+
+@Component({
+	selector: 'app',
+	templateUrl: './app.component.html',
+	providers: []
+})
+
+export class AppComponent implements OnInit {
+	constructor() {
+		console.log('AppComponent -> constructor');
+	}
+
+	ngOnInit() {
+		console.log('AppComponent -> ngOnInit');
+	}
+}
---
+++ AngularMVCApplication/App/home/home.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component, OnInit } from '@angular/core'
+
+@Component({
+	selector: 'home',
+	templateUrl: './home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+
+	constructor() {
+		console.log('HomeComponent -> constructor');
+
+	}
+
+	ngOnInit() {
+		console.log('HomeComponent -> ngOnInit');
+	}
+
+ }
